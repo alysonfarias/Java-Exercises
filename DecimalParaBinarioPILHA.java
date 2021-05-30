57f9f1cf6adc21a225cc120a2c08a94060eac4fc
Alyson Farias <63425960+alysonfarias@users.noreply.github.com>
2021-05-29 23:32:11 -0300

    Update DecimalParaBinarioPILHA.java

--- DecimalParaBinarioPILHA.java
+++ DecimalParaBinarioPILHA.java
@@ -2,6 +2,15 @@ package atvd6estruturadedados1;
 
 import java.util.Scanner;
 import java.util.Stack;
+/*Implemente um programa para converter um número, fornecido pelo
+usuário, da notação decimal para um número expresso em um sistema de numeração
+cuja base é o 2. A conversão é realizada pela divisão repetida, pela base, à qual o
+número está sendo convertido, e então se tomando os restos da divisão na ordem
+inversa. Por exemplo, a conversão para binário do número 6 exige três de tais divisões:
+6/2 = 3 com resto 0, 3/2 = 1, com resto 1 e, finalmente, 1/2 = 0 com resto 1. Os restos 0,
+1 e 1 são colocados na ordem inversa, de modo que o binário equivalente a 6 é igual a
+110.
+*/
 
 public class Atvd6EstruturaDeDados1 {
 
